e765977322a9f800560a5baa72483cb904e62882
Dinanath Maurya <88203619+dnmm@users.noreply.github.com>
2022-01-02 17:26:23 +0530

    Initial commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  var a = 10;
+  var b = 10;
+  var c = a + b;
+  print(c);
+}
---
+++ bin/variable_example.dart
@@ -0,0 +1,5 @@
+void main(List<String> arguments) {
+  var a = 100;
+  var b = 200;
+  print(a == b);
+}
