ef6e20dff437ea1f006777e6036b40ccba6ad60d
rb <707mauryaji@gmail.com>
2022-01-08 16:08:07 +0530

    list part 2 program

---
+++ bin/example_rb/list2.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<String> words = [
+    'sky',
+    'cloud',
+    'tent',
+    'tree',
+    'falcon',
+  ];
+  print(words);
+  words.remove('sky');
+  print(words);
+  words.removeLast();
+  print(words);
+  List id = [1, 2, 3, 4, 7, 9];
+  print(id);
+}
