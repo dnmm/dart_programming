7ec7c5eced6dd71118b0bd3d8e1b0db31c2a382e
rb <707mauryaji@gmail.com>
2022-01-08 15:55:49 +0530

    arithmatic_operator

---
+++ bin/example_rb/arithmatic_operation.dart
@@ -0,0 +1,31 @@
+void main() {
+  var a = 20;
+  var b = 10;
+  print("arithmatic operation in dart language");
+  print("a+b=${a + b}");
+  print("a-b=${a - b}");
+  print("a*b=${a * b}");
+  print("a/b=${a / b}");
+  print("a%b=${a % b}");
+  print("a~/b=${a ~/ b}");
+
+// arithmatic operation in other type
+
+  var x = 20;
+  var y = 10;
+  // var c;
+  // var d;
+  // var e;
+  print("arithmatic operation ");
+  //logical operation
+  var c = x + y;
+  var d = x - y;
+  var e = x * y;
+  //print process
+  print("addition of x+y=$c");
+  print("difference of x-y=$d");
+  print("multiply of x*y=$e");
+  print(c);
+  print(d);
+  print(e);
+}
