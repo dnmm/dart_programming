826a6da32d565f0400822e2859c94bf1a0d861b8
rb <707mauryaji@gmail.com>
2022-01-08 16:04:23 +0530

    for_loop operation

---
+++ bin/example_rb/for_loop.dart
@@ -0,0 +1,12 @@
+//for loop program
+void main() {
+  for (int i = 1; i <= 3; i++) {
+    print("$i-rb with serial no");
+  }
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j >= i; j++) {
+      print(j);
+    }
+  }
+}
+// ctrl+k  and ctrl+c (its commant all  all language )
