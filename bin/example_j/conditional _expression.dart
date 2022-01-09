8d6e31a1f0296d0457cf778bce01814979214809
jinalksha <jinalkshah129@gmail.com>
2022-01-09 15:45:07 +0530

    dart operators

---
+++ bin/example_j/conditional _expression.dart
@@ -0,0 +1,6 @@
+void main() {
+  var is_login = false;
+  // ignore: dead_code
+  var user = is_login ? 'Jinal' : 'Guest';
+  print(user);
+}
---
+++ bin/example_j/operators/typetest.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = "Jinal";
+  var number = 10;
+  // ignore: unnecessary_type_check
+  print(name is String);
+  // ignore: unnecessary_type_check
+  print(number is int);
+  // ignore: unnecessary_type_check
+  print(number is! int);
+}
