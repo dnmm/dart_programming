606fdf3a101b61ec0cff1cfa1cc2aac4fa3fc789
jinalksha <jinalkshah129@gmail.com>
2022-01-09 15:53:14 +0530

    string code

---
+++ bin/example_j/string/string.dart
@@ -0,0 +1,38 @@
+/*void main() {
+  //Define string in two different way
+  String name1 = 'Jinal';
+  print(name1);
+  print(name1.runtimeType);
+
+  var name2 = "Sonam";
+  print(name2);
+  print(name2.runtimeType);
+}*/
+
+/*void main() {
+  //Define string in two different way
+  var s1 = 'It\'s a language';
+  print(s1);
+}*/
+
+/*void main() {
+  //Define string in two different way
+  var s2 = "It's a language";
+  print(s2);
+}*/
+
+/*void main() {
+  //Define string in two different way
+  var mstr = '''This is
+  Multiline
+  String''';
+  print(mstr);
+}*/
+
+void main() {
+  //Define string in two different way
+  var mstr = """This is
+Multiline
+String""";
+  print(mstr);
+}
