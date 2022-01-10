cef129539c05fc21c3ea9e9918cc1342e92f769d
Sanjeev756 <88584261+Sanjeev756@users.noreply.github.com>
2022-01-10 12:35:45 +0530

    first program and variable

---
+++ bin/example_s/variable.dart
@@ -0,0 +1,14 @@
+// # Variable Name and Rules
+// # Valid Variable Name
+void main() {
+  var name = "Sanjeev";
+  var f_name = "Chandan";
+  var address1 = "Jaunpur";
+  var $money = "100";
+
+  // # Printing Variable
+  print(name);
+  print(f_name);
+  print(address1);
+  print($money);
+}
