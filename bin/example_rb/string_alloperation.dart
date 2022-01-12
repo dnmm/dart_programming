9223fb09df41162d0e36a17505620c58e398e31e
rb <707mauryaji@gmail.com>
2022-01-12 16:15:17 +0530

    string all operation

---
+++ bin/example_rb/string_alloperation.dart
@@ -0,0 +1,28 @@
+void main() {
+  var name = 'pradeep';
+  print(name.length);
+  print(name.isEmpty);
+  print(name.isNotEmpty);
+  print(name.toUpperCase());
+  print(name.toLowerCase());
+  // note
+  //use ''' or """ for mulatiple line
+  var address = '''
+  my address is -
+  post- sarnath
+  distric- varansi''';
+  print(address);
+}
+
+ /* output=
+  7
+false
+true
+PRADEEP
+pradeep
+  my address is -
+  post- sarnath
+  distric- varansi
+  */
+
+
