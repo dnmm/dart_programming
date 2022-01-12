cf78500dfc655a46f0ee2bc6b7ba2f02f3ce4a6a
rb <707mauryaji@gmail.com>
2022-01-12 16:14:13 +0530

    specific import method

---
+++ bin/example_rb/school/spacific_import.dart
@@ -0,0 +1,9 @@
+import 'student.dart' show rb, disp1;
+
+//show method use for epecefic or particular method
+//and variable
+//as willas in import method fetch all data
+void main() {
+  print(rb);
+  disp1();
+}
