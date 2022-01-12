603008ee55c765fc3bafdfcc0676c7925cfc5e16
rb <707mauryaji@gmail.com>
2022-01-12 16:10:11 +0530

    import file from samepath

---
+++ bin/example_rb/colors/import_samepath.dart
@@ -0,0 +1,15 @@
+import 'black.dart';
+import 'green.dart';
+
+void main() {
+  //object create and function coll
+  var black_obj = Black();
+  //Black is class name
+  //color is folder
+  //black.dart is file name that is in colors
+  black_obj.disp();
+  //green class
+
+  var green_obj = Green();
+  green_obj.disp();
+}
