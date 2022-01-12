b8cc07256d69315e11fc4b2fc155c6007222fe2f
rb <707mauryaji@gmail.com>
2022-01-12 16:12:02 +0530

    increment and decrement operator

---
+++ bin/example_rb/pri_postoperator.dart
@@ -0,0 +1,26 @@
+void main() {
+  print("pri increment");
+  var i = 3;
+  print(i);
+  print(++i);
+  print(i);
+
+  //post increment
+  print("post increment");
+  i = 3;
+  print(i);
+  print(i++);
+  print(i);
+  //pre dectrement
+  print("pre decrement");
+  i = 3;
+  print(i);
+  print(--i);
+  print(i);
+  //post decrement
+  print("post decrement");
+  i = 3;
+  print(i);
+  print(i--);
+  print(i);
+}
