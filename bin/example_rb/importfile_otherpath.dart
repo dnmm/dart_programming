8d3a098b794433c3951c1eb834f2b30fc459000c
rb <707mauryaji@gmail.com>
2022-01-12 16:11:02 +0530

    import file from otherpath

---
+++ bin/example_rb/importfile_otherpath.dart
@@ -0,0 +1,14 @@
+import 'colors/black.dart';
+import 'colors/green.dart';
+
+void main() {
+  //object create and function coll
+  var black_obj = Black();
+  //Black is class name
+  //black.dart file name
+  black_obj.disp();
+  //green class
+
+  var green_obj = Green();
+  green_obj.disp();
+}
