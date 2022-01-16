a7eff8cc3bac617c7afc8e836c8e30a8d0c9020a
rb <707mauryaji@gmail.com>
2022-01-16 10:58:31 +0530

    for loop program

---
+++ bin/example_rb/forloop_program.dart
@@ -0,0 +1,24 @@
+//print even number
+void main() {
+  int num = 20;
+  /*
+  for (int i = 1; i <= num; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
+*/
+//print odd number
+/*
+  for (int i = 1; i <= num;) {
+    print(i);
+    i = i + 2;
+  }
+  */
+
+  for (int i = 1; i <= num; i++) {
+    if (i % 2 == 1) {
+      print(i);
+    }
+  }
+}
