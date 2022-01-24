df1cab8d72db00a7dc55734ba5968411cd7e93d0
rahulmaurya01 <rahul9336446070@gmail.com>
2022-01-24 13:04:58 +0530

    Update arthmeticoperator.dart

--- bin/example.rahul/arthmeticoperator.dart
+++ bin/example.rahul/arthmeticoperator.dart
@@ -3,21 +3,21 @@ void main() {
   var b = 10;
   var c = a + b;
   print(c);
-}
-// void main() {
-//   var a = 20;
-//   var b = 15;
-//   var c = a - b;
-//   print(c);
 
-// void main(){
-// var a = 30;
-// var b = 20;
-// var c = a*b;
-// print(c);
 
-// void main(){
-// var a = 100;
-// var b = 20;
-// var c = a/b;
-// print(c);
+ var d = 20;
+ var e = 15;
+ var f = d - e;
+ print(f);
+ 
+ var g = 30;
+ var h = 20;
+ var i = g*h;
+ print(i);
+
+
+ var j = 100;
+ var k = 20;
+ var l = j/k;
+ print(l);
+ }
