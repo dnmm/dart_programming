59da4092d03eb32c136a3e303b410e1c29700a6c
rahulmaurya01 <rahul9336446070@gmail.com>
2022-01-24 13:09:22 +0530

    Equality and relation operator

---
+++ bin/example.rahul/equality&RelationOperator.dart
@@ -0,0 +1,16 @@
+void main() {
+  var a = 100;
+  var b = 200;
+  print(a==b);
+
+  var c = 200;
+  var d = 200;
+  print(c==d);
+
+  var e = 200;
+  var f = 200;
+  print(e>f);
+  print(e>=f);
+  print(e<=f);
+}
+
