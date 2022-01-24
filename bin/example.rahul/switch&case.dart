d4aa128568e916060681f6f9ec4d7814ceba873f
rahulmaurya01 <rahul9336446070@gmail.com>
2022-01-24 16:38:06 +0530

    switch case

---
+++ bin/example.rahul/switch&case.dart
@@ -0,0 +1,18 @@
+// # Switch and Case
+    void main(){
+    var command = 'PENDING';
+    switch (command) {
+      case 'CLOSED':
+        print('Complaint Ticket Closed');
+        break;
+      case 'PENDING':
+        print('Complaint Ticket Pending');
+        break;
+      case 'DENIED':
+        print('Complaint Ticket Denied');
+        break;
+      case 'OPEN':
+        print('Complaint Ticket Open');
+        break;
+      default:
+        print('InValid Complaint Ticket');}}
