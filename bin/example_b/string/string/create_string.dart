94dcdbd89ab75344442ccb43d699c5562099abb9
babita93 <babitasmaurya@gmail.com>
2022-02-01 21:44:53 +0530

    Create create_string.dart

---
+++ bin/example_b/string/string/create_string.dart
@@ -0,0 +1,31 @@
+void main() {
+  String name1 = 'Sonam';
+  print(name1);
+  print(name1.runtimeType);
+
+  String name2 = "Rahul";
+  print(name2);
+
+  var name3 = 'Sumit';
+  print(name3);
+  print(name3.runtimeType);
+
+  var name4 = "Sumit";
+  print(name4);
+  print(name4.runtimeType);
+
+  var s1 = 'It\'s a Language';
+  print(s1);
+
+  var s2 = "It's a Language";
+  print(s2);
+
+  var mstr2 = '''This is
+Multiline
+String ''';
+  print(mstr2);
+  var mstr1 = """This is
+Multiline
+String """;
+  print(mstr1);
+}
