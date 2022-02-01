77a38053948b8981f199c1b4384637669d9c1ef5
babita93 <babitasmaurya@gmail.com>
2022-02-01 22:05:01 +0530

    Create empty_set.dart

---
+++ bin/example_b/set/empty_set.dart
@@ -0,0 +1,22 @@
+void main() {
+  var st6 = <int>{};
+  print(st6);
+  print(st6.runtimeType);
+
+  var st7 = {}; // Its Map not Set
+  print(st7);
+  print(st7.runtimeType);
+
+  Set<String> st8 = Set();
+  print(st8);
+  print(st8.runtimeType);
+
+  Set<String> st9 = {};
+  print(st9);
+  print(st9.runtimeType);
+  Set ten = {};
+  print(ten);
+  ten.add("ram");
+  ten.add(10);
+  print(ten);
+}
