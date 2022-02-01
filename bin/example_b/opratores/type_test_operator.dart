ddf976319c9e899c11782605167f680f73c7352f
babita93 <babitasmaurya@gmail.com>
2022-02-01 21:35:40 +0530

    Create type_test_operator.dart

---
+++ bin/example_b/opratores/type_test_operator.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = "Sonam";
+  var number = 10;
+  // ignore: unnecessary_type_check
+  print(name is String);
+  // ignore: unnecessary_type_check
+  print(number is int);
+  // ignore: unnecessary_type_check
+  print(number is! int);
+}
