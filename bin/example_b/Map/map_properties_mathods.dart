d4e0d77e6559ce99c30cf6f4b3f8821b9c6ecf27
babita93 <babitasmaurya@gmail.com>
2022-02-03 17:08:03 +0530

    Create map_properties_mathods.dart

---
+++ bin/example_b/Map/map_properties_mathods.dart
@@ -0,0 +1,12 @@
+void main() {
+  var address5 = {'name': 'Sonam', 'city': 'Ranchi', 'state': 'Jharkahnd'};
+
+  print(address5.length);
+  print(address5.isEmpty);
+  print(address5.isNotEmpty);
+  print(address5.keys);
+  print(address5.values);
+  //use for searsch key and value
+  print(address5.containsKey('name'));
+  print(address5.containsValue('Rahul'));
+}
