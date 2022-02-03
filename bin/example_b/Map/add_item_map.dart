ade12f164e53ede024db05013cfdf569dfc141dd
babita93 <babitasmaurya@gmail.com>
2022-02-03 17:08:18 +0530

    Create add_item_map.dart

---
+++ bin/example_b/Map/add_item_map.dart
@@ -0,0 +1,9 @@
+void main() {
+  var address3 = {};
+  address3['name'] = 'Sonam';
+  address3['city'] = 'Ranchi';
+  address3['name'] = 'Babita';
+  address3['salry'] = 2000;
+  print(address3);
+  print(address3.runtimeType);
+}
