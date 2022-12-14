530068a576172a0fa2790b00b061e51709b8f22f
Dina Nath Maurya <dnm.dpm@gmail.com>
2022-12-14 16:10:27 +0530

    delete

--- lib/day6_stateless/android/app/src/main/kotlin/com/example/day6_stateless/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.day6_stateless
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/layout_widgets/android/app/src/main/kotlin/com/example/layout_widgets/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.layout_widgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
