73c6165a3b4d22f8779e2feb23f4ff711838ee4a
Dinakar Maurya <dnm.dpm@gmail.com>
2023-03-10 10:12:57 +0530

    class update

---
+++ bin/example_d/2023 Training/class3/class 3rd_abstract_class2.dart
@@ -0,0 +1,47 @@
+// 2.    Abstract _class———-
+
+// Abstract class :This class can contain one or more than abstract method (those method without body/implementation)
+
+// abstract class can or cannot contain abstract method but if abstract method is present in any class that definitely that class is abstract
+
+// Very imp: you cannot make object of abstract class
+
+// any class extending abstract class should give the body of abstract method define in parent class
+// Why abstract class
+
+// abstract Parentcompany()
+// {
+// method 1 { with body }
+
+// abstract method 2 automatic gear() -without body
+// }
+
+// childcompany extend parentcompany()
+// { method1 { with body}
+// method 2 automatic gear () { with body}
+// }
+
+// Example
+
+abstract class Shape {
+  void area();
+  void typeOfShape();
+}
+
+class Circle extends Shape {
+  @override
+  void area() {
+    print("area of circle is 10");
+  }
+
+  @override
+  void typeOfShape() {
+    print("the type of shape is circle");
+  }
+}
+
+void main() {
+  Circle c = new Circle();
+  c.area();
+  c.typeOfShape();
+}
---
+++ bin/example_d/2023 Training/class3/class 3rd_abstract_class3.dart
@@ -0,0 +1,32 @@
+abstract class Shape {
+  void area();
+  void typeOfShape();
+
+  void printShape() {
+    print("this is shape method");
+  }
+}
+
+class Circle extends Shape {
+  @override
+  void printShape() {
+    print("this is circle shape");
+  }
+
+  @override
+  void area() {
+    print("area of circle is 10");
+  }
+
+  @override
+  void typeOfShape() {
+    print("the type of shape is circle");
+  }
+}
+
+void main() {
+  Circle c = new Circle();
+  c.area();
+  c.typeOfShape();
+  c.printShape();
+}
