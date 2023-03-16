3c5dd2e0148258b3240a00bc7576a3ef94d6b0ea
tryeno <tryeno.work@gmail.com>
2023-03-16 17:23:44 +0530

    make example

--- bin/example_d/2023 Training/class7/exception_handling_example5.dart
+++ bin/example_d/2023 Training/class7/exception_handling_example5.dart
@@ -1,1 +1,20 @@
 //make example sum of 2 number less then 50
+
+void main() {
+  int a = 25;
+  int b = 23;
+  var c = a + b;
+  try {
+    enter_num(50);
+  } catch (e) {
+    print("$c");
+  }
+}
+
+void enter_num(int num) {
+  if (num < 57) {
+    throw new FormatException();
+  } else {
+    print('sum num less than $num');
+  }
+}
