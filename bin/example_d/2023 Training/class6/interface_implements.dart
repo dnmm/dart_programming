0451d449cc24aec9ec61d8c8faeeb12afe66769d
Dina Nath Maurya <dnm.dpm@gmail.com>
2024-03-04 11:54:59 +0530

    Update interface_implements.dart

--- bin/example_d/2023 Training/class6/interface_implements.dart
+++ bin/example_d/2023 Training/class6/interface_implements.dart
@@ -1,8 +1,6 @@
 //this is required if use implements key word in class then override all method
 class Car {
-  void printDataCar() {
-    print("i am general car");
-  }
+  void printDataCar() {}
 }
 
 class Honda implements Car {
